d018e88a8e8c016bdd93ca78e4adda824940cc72
Ayaabuelsoud <ayaabuelsoud98@gmail.com>
2021-05-27 14:38:20 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+/*--------- preloader ---------*/
+
+$(window).on('load', function () {
+    $(".loader").delay(3500).fadeOut(); 
+    //$(".loader").fadeOut();     
+    $("#preloder").delay(4000).fadeOut("slow");
+});
